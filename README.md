e08b6504772274e251f8eb49b47359af477d06cf
Pourush Kumar Gupta <pkg.21p10161@mtech.nitdgp.ac.in>
2023-10-10 10:27:35 +0530

    updated the readme file

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Perceptron
 my first perceptron project 
+
+
+# Command use in this project 
+conda env list 
+conda create -n perceptron python=3.7
+conda activate perceptron 
+pip install -r requirements.txt
+git add . 
+git commit -m "updated"
+git puch origin main 
+history 
+
+## To check different version of the code 
+git checkout versioncode 
---
+++ or.py
@@ -0,0 +1,23 @@
+from utils.model import Perceptron
+from utils.all_utils import prepare_data, save_model,save_plot
+import pandas as pd 
+import numpy as np 
+
+AND = {
+    "x1":[0,0,1,1],
+    "x2":[0,1,0,1],
+    "y":[0,0,0,1]
+}
+df=pd.DataFrame(AND)
+print(df)
+
+x,y=prepare_data(df)
+
+ETA=0.3 # Learning rate lies between 0 and 1
+EPOCHS=10
+model = Perceptron(eta=ETA,epochs=EPOCHS)
+model.fit(x,y)
+_ = model.total_loss()
+
+save_model(model,filename="and.model")
+save_plot(df,"and.png",model)
